9dc5bd69ab9fe52b3cc37e7072801c2eb96b7757
anucrao <85787659+anucrao@users.noreply.github.com>
2023-11-28 15:17:16 +0530

    Synchronizing job CPECOE from AWS Glue at 2023/11/28 09:47:16

---
+++ CPECOE/C.py
@@ -0,0 +1,32 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Amazon S3
+AmazonS3_node1701153899001 = glueContext.create_dynamic_frame.from_options(
+    format_options={
+        "quoteChar": -1,
+        "withHeader": True,
+        "separator": "\t",
+        "optimizePerformance": False,
+    },
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "paths": ["s3://cpecoeminiproject/Employee Sample Data.csv"],
+        "recurse": True,
+    },
+    transformation_ctx="AmazonS3_node1701153899001",
+)
+
+job.commit()
